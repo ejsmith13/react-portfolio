7066695e07046ff59d5816a36b4811b67b422813
ejsmith13 <ejsmithwork@gmail.com>
2021-04-20 20:39:40 -0400

    fixing bootstrap layout of projects

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -3,16 +3,18 @@ import React from "react";
 
 function Project(props) {
   return (
-    <div>
+    <div className = "row" >
       {props.projects.map((e) => {
         return (
           <figure className="col-md-6">
             <h3>{e.title}</h3>
-            <a href={e.link} target="_blank">
+            <a href={e.link} target="_blank" rel="noreferrer" >
               <img src={e.image} alt={e.title} className="img-thumbnail" />
             </a>
-            <button className="btn-secondary" href={e.gh}>
+            <button className="btn-secondary" >
+              <a href={ e.gh} target="_blank" rel="noreferrer" style={{ color: "white" }}>
               Github Repo
+              </a>
             </button>
           </figure>
         );
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,7 +3,6 @@ import Workout from "../assets/images/screenshot_workout.png";
 import Burger from "../assets/images/screenshot_burger.png";
 import Quiz from "../assets/images/screenshot_quiz1.png";
 import Password from "../assets/images/password_screenshot.png";
-import Refactor from "../assets/images/refactor.png";
 import DayPlanner from "../assets/images/screenshot_day_planner.png";
 import Holiday from "../assets/images/screenshot_holiday_saved.png";
 import Project from "../components/project";
@@ -36,12 +35,6 @@ class Projects extends Component {
         gh: "https://github.com/ejsmith13/password-maker",
       },
       {
-        title: "Marketing Refactor Page",
-        link: "https://ejsmith13.github.io/horiseon_marketing_project/",
-        image: Refactor,
-        gh: "https://github.com/ejsmith13/horiseon_marketing_project",
-      },
-      {
         title: "Day Planner",
         link: "https://ejsmith13.github.io/day_planner/",
         image: DayPlanner,
@@ -66,128 +59,9 @@ class Projects extends Component {
             <p>Click the images below to visit each site.</p>
           </section>
           <section class="row">
-            {/* <!-- Workout Crusher --> */}
-            <figure class="col-md-6">
-              <h3>Workout Logging App</h3>
-              <a
-                href="https://ancient-river-16818.herokuapp.com/"
-                target="_blank"
-              >
-                <img
-                  src={Workout}
-                  alt="Workout Crusher App Home page"
-                  class="img-thumbnail"
-                />
-              </a>
-              <button class="btn-secondary" id="workoutBtn">
-                Github Repo
-              </button>
-            </figure>
-
-
-            <figure>
+           <figure>
               <Project projects={this.state.projects} />
             </figure>
-
-            {/* <figure class="col-md-6">
-              <h3>Burger Logging App</h3>
-              <a
-                href="https://glacial-coast-49781.herokuapp.com/"
-                target="_blank"
-              >
-                <img
-                  src={Burger}
-                  alt="Burger App Screenshot"
-                  class="img-thumbnail"
-                />
-              </a>
-              <button class="btn-secondary" id="burgerBtn">
-                Github Repo
-              </button>
-            </figure>
-          </section>
-          <section class="row">
-           
-            <figure class="col-md-6">
-              <h3>Coding Quiz</h3>
-              <a
-                href="https://ejsmith13.github.io/coding_quiz/"
-                target="_blank"
-              >
-                <img src={Quiz} alt="Coding Quiz page" class="img-thumbnail" />
-              </a>
-              <button class="btn-secondary" id="quizBtn">
-                Github Repo
-              </button>
-            </figure>
-            
-            <figure class="col-md-6">
-              <h3>Password Generator</h3>
-              <a
-                href="https://ejsmith13.github.io/password-maker/"
-                target="_blank"
-              >
-                <img
-                  src={Password}
-                  alt="password generator"
-                  class="img-thumbnail"
-                />
-              </a>
-              <button class="btn-secondary" id="passwordBtn">
-                Github Repo
-              </button>
-            </figure>
-          </section>
-          <section class="row">
-           
-            <figure class="col-md-6">
-              <h3>Marketing Refactor Page</h3>
-              <a
-                href="https://ejsmith13.github.io/horiseon_marketing_project/"
-                target="_blank"
-              >
-                <img
-                  src={Refactor}
-                  alt="refactor project"
-                  class="img-thumbnail"
-                />
-              </a>
-              <br />
-              <button class="btn-secondary" id="refactorBtn">
-                Github Repo
-              </button>
-            </figure>
-            
-            <figure class="col-md-6">
-              <h3>Day Planner</h3>
-              <a
-                href="https://ejsmith13.github.io/day_planner/"
-                target="_blank"
-              >
-                <img src={DayPlanner} alt="Day Planner" class="img-thumbnail" />
-              </a>
-              <button class="btn-secondary" id="plannerBtn">
-                Github Repo
-              </button>
-            </figure>
-          </section>
-          <section class="row">
-            <figure class="col-md-6">
-              <h3>Holiday Movie Finder</h3>
-              <a
-                href="https://vpatt1031.github.io/movie_roullette/"
-                target="_blank"
-              >
-                <img
-                  src={Holiday}
-                  alt="Holiday Movie Finder App"
-                  class="img-thumbnail"
-                />
-              </a>
-              <button class="btn-secondary" id="holidayBtn">
-                Github Repo
-              </button>
-            </figure> */}
           </section>
         </main>
       </div>
